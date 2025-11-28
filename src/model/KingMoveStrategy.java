53088dc32ab43d26d0e53662ba34bef27a6f23ed
Egor Mineailo <egor.mineailo@stud.hwc.ac.at>
2025-11-28 17:47:48 +0100

    test

---
+++ src/model/KingMoveStrategy.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.List;
+
+public class KingMoveStrategy implements IMoveStrategy {
+ // 123
+    @Override
+    public List<Position> getMoves(Piece piece, Board board) {
+        return List.of();
+    }
+}
