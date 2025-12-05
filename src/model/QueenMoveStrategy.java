6bb349ffaead7e5ae43208f84a65a3f837dbc8e6
abd2716 <rawan.abd.el.galil@stud.hcw.ac.at>
2025-12-05 16:31:01 +0100

    queen update

--- src/model/QueenMoveStrategy.java
+++ src/model/QueenMoveStrategy.java
@@ -1,10 +1,24 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class QueenMoveStrategy implements IMoveStrategy{
+public class QueenMoveStrategy implements IMoveStrategy {
+
     @Override
     public List<Position> getMoves(Piece piece, Board board) {
-        return List.of();
+        List<Position> validMoves = new ArrayList<>();
+
+        // queen moves like rook + bishop
+        RookMoveStrategy rookStrategy = new RookMoveStrategy();
+        BishopMoveStrategy bishopStrategy = new BishopMoveStrategy();
+
+        // rook moves
+        validMoves.addAll(rookStrategy.getMoves(piece, board));
+
+        // bishop moves
+        validMoves.addAll(bishopStrategy.getMoves(piece, board));
+
+        return validMoves;
     }
 }
