e5dbfad1d4b5a17a1adb248415bf16755d9a1f59
abd2716 <rawan.abd.el.galil@stud.hcw.ac.at>
2025-12-14 14:19:44 +0100

    commit

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -93,7 +93,6 @@ public class GameState {
         switchTurn();
         statusMessage = "Undo successful! " + currentTurn + " to move.";
     }
-
     // --- Helper Methods ---
 
     private Position findKing(Color currentColor){
